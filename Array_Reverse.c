0bc887f4818955dfa1f7395aa017959de53cf35b
Kamal Khandelwal <kamal8.khandelwal@gmail.com>
2026-01-08 16:55:54 +0530

    Add files via upload

---
+++ Array_Reverse.c
@@ -0,0 +1,18 @@
+/*WAP to read n number of values in an array and display them in reverse*/
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Elements in reverse order: ");
+    for (int i = n - 1; i >= 0; i--) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
